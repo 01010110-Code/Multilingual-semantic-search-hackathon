a335cdff2cd66298dc0854a5d68306f20f10db20
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-13 02:40:33 +0100

    imported Cohere & Qrant, transformed our sample text into vectors using the Cohere API

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import
+import cohere
+from qdrant_client import QdrantClient
+
+
+client = cohere.Client(api_key='9HgYT8dxqVVBTC4iNp9OS8shPD5cbKWgWTZNaTOp')
+
+document = "This is a sample document"
+
+embedding = client.embed(texts=[document])
+print(f"Embedding for the document '{document}': {embedding} ")
+
