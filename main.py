5b92670642684c3b854fa8a58ee417befdd98f0f
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-13 02:49:49 +0100

    m

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from qdrant_client import QdrantClient
 cohere_client = cohere.Client(api_key='9HgYT8dxqVVBTC4iNp9OS8shPD5cbKWgWTZNaTOp')
 
 # Initialize Qdrant client
-qdrant_client = QdrantClient(api_key="nNWdYRTYLSdw2PMChu2dlRYC67Ja-77_DGh-kyb8aa5BxuwFVXFP9Q", api_url="44e6fb22-9094-444a-9a69-4feaf6eb5b94.us-east-1-0.aws.cloud.qdrant.io")
+qdrant_client = QdrantClient(api_key="nNWdYRTYLSdw2PMChu2dlRYC67Ja-77_DGh-kyb8aa5BxuwFVXFP9Q", api_url="https://44e6fb22-9094-444a-9a69-4feaf6eb5b94.us-east-1-0.aws.cloud.qdrant.io")
 
 # Define index configuration
 index_config = {
@@ -25,7 +25,7 @@ qdrant_client.create_index(index_name=index_name, index_config=index_config)
 
 # Embed the document using Cohere
 document = "This is a sample document"
-embedding = cohere_client.embed(texts=[document])[0]  # Cohere returns a list of embeddings
+embedding = cohere_client.embed(texts=[document]).embeddings[0]  # Cohere returns an embeddings object, so we need to access the embeddings list
 
 # Convert the Cohere embedding to the format expected by Qdrant
 embedding_dict = {"id": 1, "vector": embedding}
@@ -34,7 +34,7 @@ embedding_dict = {"id": 1, "vector": embedding}
 qdrant_client.add_documents(index_name=index_name, documents=[embedding_dict])
 
 query_embedding = [0.1, 0.2, ..., 0.5]
-search_result = client.search(
+search_result = qdrant_client.search(
     index_name=index_name,
     query={
         "vector": query_embedding,
