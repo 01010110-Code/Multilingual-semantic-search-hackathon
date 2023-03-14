5eb5e9aaa6da4a9cb25466357a010c79d933ddfb
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-14 03:55:47 +0100

    added the script that preprocess and process documents

---
+++ preprocess_script.py
@@ -0,0 +1,18 @@
+import spacy
+import os
+
+nlp = spacy.load('en_core_web_sm')
+
+def preprocess_documents(directory):
+    """
+    This function reads all the text files in a directory and preprocesses them using the SpaCy library.
+    """
+    docs = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            with open(os.path.join(directory, filename), 'r', encoding='utf-8') as f:
+                text = f.read()
+                doc = nlp(text)
+                tokens = [token.text.lower() for token in doc if not token.is_stop and not token.is_punct and not token.like_num]
+                docs.append(tokens)
+    return docs
