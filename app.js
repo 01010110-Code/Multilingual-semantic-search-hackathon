c14a16f6e9585123dcb6ec4e216d862126427518
V <89860338+01010110-Code@users.noreply.github.com>
2023-03-24 20:48:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 
-// we are going to use Bootstrap, Jquery, Saas and react in the front end of this project  - init//
+// we are going to use Bootstrap and  React in the front end of this project  - init//
